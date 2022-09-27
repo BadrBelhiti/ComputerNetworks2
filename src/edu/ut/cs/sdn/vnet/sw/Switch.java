4ed130579cf1d2871930cbb82a4c4e501987a3f7
Rithik Batchu <rithikbatchu1@gmail.com>
2022-09-27 12:14:46 -0500

    initial switchTable implementation

--- src/edu/ut/cs/sdn/vnet/sw/Switch.java
+++ src/edu/ut/cs/sdn/vnet/sw/Switch.java
@@ -8,30 +8,74 @@ import edu.ut.cs.sdn.vnet.Iface;
 /**
  * @author Aaron Gember-Jacobson
  */
-public class Switch extends Device
-{	
+public class Switch extends Device {
+
+	private HashMap<CachableMACAddress, Iface> switchTable;
+
 	/**
 	 * Creates a router for a specific host.
+	 * 
 	 * @param host hostname for the router
 	 */
-	public Switch(String host, DumpFile logfile)
-	{
-		super(host,logfile);
+	public Switch(String host, DumpFile logfile) {
+		super(host, logfile);
 	}
 
 	/**
 	 * Handle an Ethernet packet received on a specific interface.
+	 * 
 	 * @param etherPacket the Ethernet packet that was received
-	 * @param inIface the interface on which the packet was received
+	 * @param inIface     the interface on which the packet was received
 	 */
-	public void handlePacket(Ethernet etherPacket, Iface inIface)
-	{
+	public void handlePacket(Ethernet etherPacket, Iface inIface) {
 		System.out.println("*** -> Received packet: " +
-                etherPacket.toString().replace("\n", "\n\t"));
-		
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
-		
-		/********************************************************************/
+				etherPacket.toString().replace("\n", "\n\t"));
+
+		CachableEntry entry = switchTable.get(etherPacket.getDestinationMAC());
+		if (entry != null && !entry.isExpired()) {
+			entry.resetExpirationTime();
+			if (entry.getIface() != inIface) {
+				sendPacket(etherPacket, entry.getIface());
+			}
+		} else {
+			if (entry != null) {
+				switchTable.remove(etherPacket.getDestinationMAC());
+			}
+			for (Iface iface : interfaces.values()) {
+				if (iface != inIface) {
+					sendPacket(etherPacket, iface);
+				}
+			}
+		}
+
+		switchTable.put(etherPacket.getSourceMAC(), new CachableEntry(inIface));
+
+	}
+
+}
+
+private class CachableEntry {
+	private IFace outIface;
+	private long expirationTime;
+
+	public CachableEntry(Iface iface) {
+		this.outIface = iface;
+		this.expirationTime = System.currentTimeMillis() + 15000; // 15 seconds from the current time
+	}
+
+	public MACAddress getIFace() {
+		return outIface;
+	}
+
+	public long getExpirationTime() {
+		return expirationTime;
+	}
+
+	public boolean isExpired() {
+		return System.currentTimeMillis() > expirationTime;
+	}
+
+	public void resetExpirationTime() {
+		expirationTime = System.currentTimeMillis() + 15000;
 	}
 }
